759aeb3643674180481a128a5205208093df97db
Adam Fine <adamfine@outlook.com>
2023-05-24 19:59:10 -0400

    commit 2

--- src/app/convert.pipe.ts
+++ src/app/convert.pipe.ts
@@ -5,8 +5,13 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class ConvertPipe implements PipeTransform {
 
-  transform(value: unknown, ...args: unknown[]): unknown {
-    return null;
+  transform(value: any, ...args: any[]): any {
+
+    if(!value) {
+      return ''
+    }
+
+    return value * 1.60934;
   }
 
 }
